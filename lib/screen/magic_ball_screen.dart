530ae260a53ec076fb4487facea5cc3a53c1e953
iljaKorn <kornilov.ilja@rambler.ru>
2023-07-25 13:28:23 +0300

    Add Circle on screen without animation

--- lib/screen/magic_ball_screen.dart
+++ lib/screen/magic_ball_screen.dart
@@ -5,6 +5,38 @@ class MagicBallScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.indigo, Colors.black])),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Padding(padding: EdgeInsets.only(top: 200)),
+                  ElevatedButton(
+                    child: Text("Нажми на меня"),
+                    style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(), fixedSize: Size(300, 300)),
+                    onPressed: () {},
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 50)),
+                  ElevatedButton(
+                      onPressed: () {}, child: Text("Тут будет овал")),
+                  Padding(padding: EdgeInsets.only(top: 60)),
+                  Text(
+                    "Нажмите на шар",
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          )),
+    );
   }
 }
